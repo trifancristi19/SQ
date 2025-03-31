a3f735f3653834ee9e24810d0372c2d77960e1d3
trifancristi19 <cristian.trifan@student.nhlstenden.com>
2025-03-31 16:56:16 +0200

    Added my part related to design patterns

---
+++ src/main/java/com/jabberpoint/SlideItemFactory.java
@@ -0,0 +1,17 @@
+package com.jabberpoint;
+
+/**
+ * Factory for creating different types of SlideItems
+ */
+public class SlideItemFactory {
+    public static SlideItem createSlideItem(String type, int level, String content) {
+        switch (type.toLowerCase()) {
+            case "text":
+                return new TextItem(level, content);
+            case "bitmap":
+                return new BitmapItem(level, content);
+            default:
+                throw new IllegalArgumentException("Unknown slide item type: " + type);
+        }
+    }
+} 
--- src/main/java/com/jabberpoint/SlideViewerComponent.java
+++ src/main/java/com/jabberpoint/SlideViewerComponent.java
@@ -5,9 +5,8 @@ import java.awt.Font;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.image.ImageObserver;
 import javax.swing.JComponent;
-import javax.swing.JFrame;
-
 
 /** <p>SlideViewerComponent is a graphical component that can show slides.</p>
  * @author Ian F. Darwin, ian@darwinsys.com, Gert Florijn, Sylvia Stuurman
@@ -19,12 +18,11 @@ import javax.swing.JFrame;
  * @version 1.6 2014/05/16 Sylvia Stuurman
  */
 
-public class SlideViewerComponent extends JComponent {
+public class SlideViewerComponent extends JComponent implements PresentationObserver {
 		
-	private transient Slide slide; // current slide
+	private Slide slide; // current slide
 	private Font labelFont; // font of labels
-	private transient Presentation presentation = null; // the presentation
-	private JFrame frame = null;
+	private Presentation presentation; // the presentation
 	
 	private static final long serialVersionUID = 227L;
 	
@@ -32,37 +30,37 @@ public class SlideViewerComponent extends JComponent {
 	private static final Color COLOR = Color.black;
 	private static final String FONTNAME = "Dialog";
 	private static final int FONTSTYLE = Font.BOLD;
-	private static final int FONTHEIGHT = 10;
+	private static final int FONTSIZE = 10;
 	private static final int XPOS = 1100;
 	private static final int YPOS = 20;
 
-	public SlideViewerComponent(Presentation pres, JFrame frame) {
-		presentation = pres;
-		labelFont = new Font(FONTNAME, FONTSTYLE, FONTHEIGHT);
-		this.frame = frame;
+	public SlideViewerComponent(Presentation presentation) {
 		setBackground(BGCOLOR);
+		labelFont = new Font(FONTNAME, FONTSTYLE, FONTSIZE);
+		this.presentation = presentation;
+		this.presentation.addObserver(this);
 	}
 
-	public void initialize() {
-		// Background already set in constructor
+	@Override
+	public void onSlideChanged(int slideNumber) {
+		this.slide = presentation.getSlide(slideNumber);
+		repaint();
+	}
+
+	@Override
+	public void onPresentationChanged() {
+		if (presentation.getSize() > 0) {
+			this.slide = presentation.getCurrentSlide();
+		} else {
+			this.slide = null;
+		}
+		repaint();
 	}
 
 	public Dimension getPreferredSize() {
 		return new Dimension(Slide.WIDTH, Slide.HEIGHT);
 	}
 
-	public void update(Presentation presentation, Slide data) {
-		if (data == null) {
-			repaint();
-			return;
-		}
-		this.presentation = presentation;
-		this.slide = data;
-		repaint();
-		frame.setTitle(presentation.getTitle());
-	}
-
-// draw the slide
 	public void paintComponent(Graphics g) {
 		g.setColor(BGCOLOR);
 		g.fillRect(0, 0, getSize().width, getSize().height);
@@ -71,8 +69,7 @@ public class SlideViewerComponent extends JComponent {
 		}
 		g.setFont(labelFont);
 		g.setColor(COLOR);
-		g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " +
-                 presentation.getSize(), XPOS, YPOS);
+		g.drawString("Slide " + (1 + presentation.getSlideNumber()) + " of " + presentation.getSize(), XPOS, YPOS);
 		Rectangle area = new Rectangle(0, YPOS, getWidth(), (getHeight() - YPOS));
 		slide.draw(g, area, this);
 	}
