9900b20b5e6222af49be295d98b477ef58c7d8b4
trifancristi19 <cristian.trifan@student.nhlstenden.com>
2025-04-03 16:46:26 +0200

    Removed the UI Tests and made them more clear

--- src/test/java/com/jabberpoint/AboutBoxTest.java
+++ src/test/java/com/jabberpoint/AboutBoxTest.java
@@ -15,12 +15,6 @@ public class AboutBoxTest {
             return;
         }
         
-        // We can't verify the actual message dialog in a headless environment,
-        // but we can at least verify that the method doesn't throw an exception
-        // when called with an empty frame.
-        
-        // This is a very minimal test - ideally we would use a mocking framework
-        // to verify the JOptionPane was called with the right parameters.
         try {
             AboutBox.show(null); // Use null instead of Frame to avoid HeadlessException
             assertTrue(true); // If we made it here, no exception was thrown
