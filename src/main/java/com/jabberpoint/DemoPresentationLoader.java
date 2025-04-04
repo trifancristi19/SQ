2d2c1674f355d5b5670e5131c9241c976af79014
Mihail177 <mihail.josan@student.nhlstenden.com>
2025-04-04 19:08:08 +0200

    Fixed the issue

--- src/main/java/com/jabberpoint/DemoPresentationLoader.java
+++ src/main/java/com/jabberpoint/DemoPresentationLoader.java
@@ -1,16 +1,23 @@
 package com.jabberpoint;
 
+import com.jabberpoint.io.DemoPresentationReader;
+
 /**
  * Demo implementation of PresentationLoader
  * @deprecated Use com.jabberpoint.io.DemoPresentationReader instead
  */
 @Deprecated
 public class DemoPresentationLoader implements PresentationLoader {
-    private final DemoPresentation demoPresentation = new DemoPresentation();
+    private final DemoPresentationReader demoReader = new DemoPresentationReader();
 
     @Override
     public void loadPresentation(Presentation presentation, String fileName) {
-        demoPresentation.loadFile(presentation, fileName);
+        try {
+            demoReader.loadPresentation(presentation, fileName);
+        } catch (Exception e) {
+            // Maintain backward compatibility by not throwing exception
+            System.err.println("Error loading demo presentation: " + e.getMessage());
+        }
     }
 
     @Override
--- src/main/java/com/jabberpoint/io/DemoPresentationLoader.java
+++ src/main/java/com/jabberpoint/io/DemoPresentationLoader.java
@@ -1,17 +1,16 @@
 package com.jabberpoint.io;
 
 import com.jabberpoint.Presentation;
-import com.jabberpoint.DemoPresentation;
 
 /**
  * Demo implementation of PresentationReader
  * Only implements reader interface following LSP - does not pretend to be writable
  */
 public class DemoPresentationLoader implements PresentationReader {
-    private final DemoPresentation demoPresentation = new DemoPresentation();
+    private final DemoPresentationReader demoReader = new DemoPresentationReader();
     
     @Override
     public void loadPresentation(Presentation presentation, String fileName) throws Exception {
-        demoPresentation.loadFile(presentation, fileName);
+        demoReader.loadPresentation(presentation, fileName);
     }
 } 
